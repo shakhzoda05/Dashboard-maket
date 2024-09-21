273ca5c4940ffa9dd0bf860661cb8c777c1a91a1
shakhzoda05 <shakhzoda05@gmail.com>
2024-09-22 02:34:06 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppRouter from './Router/AppRouter';
+import './index.css'; 
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
